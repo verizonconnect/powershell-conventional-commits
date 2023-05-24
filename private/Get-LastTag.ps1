fd4478016211a00d3ce6589ef963b144ef608f0a
Jon Vaughan <vaughanie1978@gmail.com>
2023-05-24 18:46:40 +0100

    fix: dubious ownership error.

--- private/Get-LastTag.ps1
+++ private/Get-LastTag.ps1
@@ -14,9 +14,26 @@ function Get-LastTag {
         $returnedTag = $currentTag
     }
     elseif ($LASTEXITCODE -eq 128) {
-        #$currentTag now holds the error
-        Write-Debug "While getting the last tag; the following error occured ""$currentTag"""
-        $returnedTag = ''
+        if ($currentTag -Like "*dubious ownership*") {
+
+            Write-Debug "Error caught dubious ownership, adding this path to the safe.directory."
+
+            Invoke-Expression "git config --global --add safe.directory $PWD"
+
+            $currentTag = Invoke-Expression "git describe --tags --match=$MatchPattern --exclude=$ExcludePattern --no-abbrev $CommitAnchor *>&1" 
+
+            if ($LASTEXITCODE -eq 0) {
+                $returnedTag = $currentTag
+            }
+            else {
+                Write-Debug "While getting the last tag; the following error occured ""$currentTag"""
+                $returnedTag = ''
+            }
+        }
+        else {
+            Write-Debug "While getting the last tag; the following error occured ""$currentTag"""
+            $returnedTag = ''
+        }
     }
     else {
         throw "error while calling GIT describe"
